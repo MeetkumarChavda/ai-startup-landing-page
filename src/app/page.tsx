c1d0a8a21267d4c17ec3d9292d91b4fcebf07c7a
Meetkumar Chavda <meetkumar.chavda785@gmail.com>
2024-08-26 18:17:05 +0530

    feat:Footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,19 @@ import { LogoTicker } from "@/sections/LogoTicker";
 import { Features } from "@/sections/Features";
 import { Testimonials } from "@/sections/Testimonials";
 import { CallToAction } from "@/sections/CallToAction";
+import { Footer } from "@/sections/Footer";
+
 export default function Home() {
-  return <>
+  return (
+  <>
     <Header />
     <Hero />
     <LogoTicker />
     <Features />
     <Testimonials />
     <CallToAction />
-  </>;
-
+    <Footer />
+  </>
+);
 }
 
