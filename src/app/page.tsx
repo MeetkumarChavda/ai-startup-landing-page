4a418ad9a9e890f992ba38f83360c7989acccb7d
Meetkumar Chavda <meetkumar.chavda785@gmail.com>
2024-08-26 15:15:15 +0530

    feat: Testimonials section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { Features } from "@/sections/Features";
+import { Testimonials } from "@/sections/Testimonials";
 export default function Home() {
   return <>
     <Header />
     <Hero />
     <LogoTicker />
     <Features />
+    <Testimonials />
   </>;
+
 }
 
