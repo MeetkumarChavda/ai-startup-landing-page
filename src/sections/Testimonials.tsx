cad89624eb9ad446f5fd8283e9cdb18c451c0f6a
Meetkumar Chavda <meetkumar.chavda785@gmail.com>
2024-08-27 15:24:34 +0530

    feat:Marquee motion to Testimonials sections

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,10 @@
+'use client'
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const testimonials = [
   {
@@ -39,9 +41,21 @@ export const Testimonials = () => {
       <p className="text-white/70 text-lg md:text-xl max-w-sm mx-auto text-center mt-5 tracking-tight">
         Our revolutionary AI SEO tools have transformed our clients stategies.
       </p>
-      <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-        <div className="flex gap-5">
-          {testimonials.map((testimonial)=>(
+      <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+        <motion.div 
+          initial={{
+            translateX:`-50%`,
+          }}
+          animate={{
+            translateX:`0%`,
+          }}
+          transition={{
+            ease:'linear',
+            repeat:Infinity,
+            duration:30,
+          }}
+          className="flex gap-5 pr-5 flex-none ">
+          {[...testimonials,...testimonials].map((testimonial)=>(
             <div key={testimonial.name} className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none" >
               <div className="text-lg md:text-xl tracking-tight">
                 {testimonial.text}
@@ -60,9 +74,8 @@ export const Testimonials = () => {
                 </div>
               </div>
             </div>
-
           ))}
-        </div>
+        </motion.div>add .
       </div>
     </div>
     
